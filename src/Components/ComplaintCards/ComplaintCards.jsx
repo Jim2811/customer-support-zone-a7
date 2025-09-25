b49508e7b033928c52e6af65013697c4b360b6f1
Rukhsat <rhjim2005@gmail.com>
2025-09-25 22:05:23 +0600

    Responsive Cards Container

--- src/Components/ComplaintCards/ComplaintCards.jsx
+++ src/Components/ComplaintCards/ComplaintCards.jsx
@@ -4,21 +4,21 @@ const ComplaintCards = ({complaints}) => {
     const complaintDatas = use(complaints)
     return (
         <>
-            <div className='mw-12 flex justify-between p-3'>
-                <div>
-                    <h2>Customer Tickets</h2>
-                    <div>
+            <div className='mw-12 flex justify-between p-3 gap-4 flex-col md:flex-row'>
+                <div className='md:w-[80%]'>
+                    <h2 className='font-bold text-2xl mb-4'>Customer Tickets</h2>
+                    <div className='grid m:grid-cols-2 gap-3 '>
                         {
                             complaintDatas.map(complaint => <ComplaintCard complaint = {complaint} key={complaint.id}></ComplaintCard>)
                         }
                     </div>
                 </div>
-                <div>
+                <div className='md:w-[30%]pl-4'>
                     <div>
-                        Task Status
+                        <h2 className='font-bold text-2xl mb-4'>Task Status</h2>
                     </div>
                     <div>
-                        Resolved Task
+                        <h2 className='font-bold text-2xl'>Resolved Task</h2>
                     </div>
                 </div>
             </div>
