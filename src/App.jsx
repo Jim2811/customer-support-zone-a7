72ef4a72444ba753115d61655f9939daa9ec260a
Rukhsat <rhjim2005@gmail.com>
2025-09-25 20:15:51 +0600

    fetched complaints

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { Suspense } from 'react'
 import './App.css'
+import ComplaintCards from './Components/ComplaintCards/ComplaintCards'
 import Nav from './Components/Nav/Nav'
 import PRCards from './Components/PRCards/PRCards'
 function App() {
-
+  const complaints = fetch('/complaint.json').then(r => r.json())
   return (
     <>
       <Nav></Nav>
       <PRCards></PRCards>
+      <Suspense>
+        <ComplaintCards complaints = {complaints}></ComplaintCards>
+      </Suspense>
     </>
   )
 }
---
+++ src/Components/ComplaintCards/ComplaintCards.jsx
@@ -0,0 +1,13 @@
+import React, { use } from 'react';
+
+const ComplaintCards = ({complaints}) => {
+    const complaintDatas = use(complaints)
+    console.log(complaintDatas);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ComplaintCards;
